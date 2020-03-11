8202f4dd109b369b362101a0c638d63855849c3c
Simone Tarantino <questamailnonmiserveaniente@gmail.com>
2020-03-11 16:02:57 +0100

    Scrittura modello

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+  //modello
+
+  //dipendente
+    //private $id
+    //private $nome
+    //private $cognome
+    //private $dataDiNascita
+    //private $ruolo
+    //private $contrattoId
+    //private $orarioLavorativo
+    // __construct($_nome, $_cognome, $_tipoDiContratto)
+
+    //responsabile extend dipendente
+      //private $settore
+      // __construct($_nome, $_cognome, $_tipoDiContratto)
+        //parent::__construct($_nome, $_cognome, $_tipoDiContratto)
+
+    //dirigente extend dipendente
+    // __construct($_nome, $_cognome, $_tipoDiContratto)
+      //parent::__construct($_nome, $_cognome, $_tipoDiContratto)
+
+  //contratto
+    //private $id
+    //private $tipo
+    //private $salario
+    //private $dataInizio
+    //private $dataFine
+    //private $durata
